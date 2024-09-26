e3fdfb130a784b45528effa0653052ada1b29579
Manuel <wtfjoke@livenet.ch>
2024-09-27 00:48:39 +0200

    ♻️ Favor casting over ts-ignore

--- src/stream-unmarshaller.ts
+++ src/stream-unmarshaller.ts
@@ -1,25 +1,31 @@
+import type { AttributeValue } from "@aws-sdk/client-dynamodb";
 import { unmarshall } from "@aws-sdk/util-dynamodb";
-import type {
-    DynamoDBRecord,
-} from "aws-lambda";
+import type { DynamoDBRecord } from "aws-lambda";
 
-export const handler = async (event: DynamoDBRecord[]): Promise<DynamoDBRecord[]> => {
+export const handler = async (
+    event: DynamoDBRecord[],
+): Promise<DynamoDBRecord[]> => {
     console.log("Received event:", JSON.stringify(event, null, 2));
-    const processedRecords: DynamoDBRecord[] = event.map(record => {
-        // @ts-ignore - its the same type from different packages (aws-lambda vs @aws-sdk/client-dynamodb)
-        const NewImage = record.dynamodb?.NewImage ? unmarshall(record.dynamodb?.NewImage) : undefined;
-        // @ts-ignore - its the same type from different packages (aws-lambda vs @aws-sdk/client-dynamodb)
-        const OldImage = record.dynamodb?.OldImage ? unmarshall(record.dynamodb?.OldImage) : undefined;
+    const processedRecords: DynamoDBRecord[] = event.map((record) => {
+        const dynamodb = record.dynamodb;
+
+        const NewImage = dynamodb?.NewImage
+            ? unmarshall(dynamodb.NewImage as Record<string, AttributeValue>) // casting here because its the same type from different packages (aws-lambda vs @aws-sdk/client-dynamodb)
+            : undefined;
+
+        const OldImage = dynamodb?.OldImage
+            ? unmarshall(dynamodb.OldImage as Record<string, AttributeValue>) // casting here because its the same type from different packages (aws-lambda vs @aws-sdk/client-dynamodb)
+            : undefined;
 
         return {
             ...record,
             dynamodb: {
                 ...record.dynamodb,
                 NewImage,
-                OldImage
+                OldImage,
             },
         };
     });
     console.log(JSON.stringify(processedRecords, null, 2));
     return processedRecords;
-}
+};
